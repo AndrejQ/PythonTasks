20f654d8e679c4d693dc7ada658482ac9777e0bd
Andrey <ponorenko@yandex.ru>
2018-03-05 23:06:24 +0300

    particle trase blocked by cylinder surface

--- NumericalMethods/emitter_in_cyllinder.py
+++ NumericalMethods/emitter_in_cyllinder.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
-from numpy import array, cos, sin, sqrt, log, arctan, pi, linspace, meshgrid
+from numpy import array, cos, sin, tan, sqrt, log, arctan, pi, linspace, meshgrid
 from numpy.random import random, uniform
 from mpl_toolkits.mplot3d import Axes3D
+
+particles = 100
 
 
 # returns array of pair of dots
@@ -28,10 +30,10 @@ def emitter(n, xtr, ytr):
 
 
 def end_point(x, y, z):
-    r = 30
-    sigma = 1
-    L = - 10 * log(random()) / sigma
-    L = 2
+    r = 35
+    sigma = 0.1
+    L = - log(random()) / sigma
+    L = 50
     phi = uniform(0, 2 * pi)
     ksi = pi - arctan(y / x)
     l_1 = sqrt(x * x + y * y)
@@ -43,22 +45,49 @@ def end_point(x, y, z):
     alpha = uniform(0, pi / 2)
     x_end = x + L * cos(alpha) * cos(phi)
     y_end = y + L * cos(alpha) * sin(phi)
-    # need fixes for l_2
-    # x_end = x + l_2 * cos(phi)
-    # y_end = y + l_2 * sin(phi)
     z_end = L * sin(alpha)
-    if alpha < arctan(20 / l_2):
-        if L * cos(alpha) > l_2:
-            x_end *= l_2 / (L * cos(alpha))
-            y_end *= l_2 / (L * cos(alpha))
-            z_end *= l_2 / (L * cos(alpha))
-    else:
-        if z_end > 20:
-            x_end *= 20 / z_end
-            y_end *= 20 / z_end
-            z_end *= 20 / z_end
-    # print(x_end, y_end, x + l_2 * cos(phi), y + l_2 * sin(phi))
+
+    x_y_end = binary_shortener(x, y, phi, r)
+    l_2 = sqrt((x - x_y_end[0]) ** 2 + (y - x_y_end[1]) ** 2)
+    if x_end ** 2 + y_end ** 2 > r ** 2 or z_end > 20:
+        if tan(alpha) < (20 / l_2):
+            z_end *= abs(l_2 / (L * cos(alpha)))
+            x_end = x_y_end[0]
+            y_end = x_y_end[1]
+            # if L * cos(alpha) > l_2:
+            #     x_end *= l_2 / (L * cos(alpha))
+            #     y_end *= l_2 / (L * cos(alpha))
+            #     z_end *= l_2 / (L * cos(alpha))
+        else:
+            if z_end > 20:
+                x_end *= 20 / z_end
+                y_end *= 20 / z_end
+                z_end *= 20 / z_end
+            if x_end ** 2 + y_end ** 2 > r ** 2:
+                podobie = l_2 / ((x - x_end) ** 2 + (y - y_end) ** 2)
+                z_end *= podobie
+                x_end *= podobie
+                y_end *= podobie
+
     return [x_end, y_end, z_end]
+
+
+def binary_shortener(x, y, phi, r):
+    d = 71
+    d_previous = 0
+    eps = 0.00001
+    x_new = x + d * cos(phi)
+    y_new = y + d * sin(phi)
+    while abs(x_new ** 2 + y_new ** 2 - r ** 2) > eps:
+        if x_new ** 2 + y_new ** 2 > r ** 2:
+            d = d - (d - d_previous) / 2
+            x_new = x - d * cos(phi)
+            y_new = y - d * sin(phi)
+        else:
+            d, d_previous = d + (d - d_previous) / 2, d
+            x_new = x - d * cos(phi)
+            y_new = y - d * sin(phi)
+    return [x_new, y_new]
 
 
 def is_in_triangle(x, y, xtr, ytr):
@@ -92,11 +121,13 @@ Xc, Zc = meshgrid(x, z)
 Yc = 35 * sqrt(1 - (Xc / 35) ** 2)
 
 # lines
-for pair in emitter(10, Xtr, Ytr):
+emitted_particles = emitter(particles, Xtr, Ytr)
+for pair in emitted_particles:
     X = pair[0]
     Y = pair[1]
     Z = pair[2]
     ax.plot(X, Y, Z, color='green')
+
 
 # Draw parameters
 rstride = 20
@@ -107,4 +138,15 @@ ax.plot_surface(Xc, -Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride)
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
+
+
+# # draw 2d triangle
+fig_2d = plt.figure()
+plt.plot(Xtr, Ytr)
+plt.plot([Xtr[0], Xtr[2]], [Ytr[0], Ytr[2]], color='blue')
+x_start_coords = [i[0][0] for i in emitted_particles]
+y_start_coords = [i[1][0] for i in emitted_particles]
+plt.plot(x_start_coords, y_start_coords, 'o')
+
+
 plt.show()
