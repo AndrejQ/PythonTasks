67e9bd690b587bb273988037c866ad8f0776ff62
Andrey <ponorenko@yandex.ru>
2018-05-27 13:55:27 +0300

    diploma work, model of bragg mirror

--- NumericalMethods/diplom/wave_propagation.py
+++ NumericalMethods/diplom/wave_propagation.py
@@ -17,21 +17,24 @@ def wave_propagate(position, wave, depth):
             n1 = n[position][0]
             n2 = n[position + 1][0]
             # propagate ->
-            wave_propagate(position + 1, [wave[0],
+            wave_propagate(position + 1, [wave[0] * 2 * n1 / (n1 + n2),
                                           wave[1] / n1 * n2,
                                           wave[2] + points[position + 1] * (wave[1] - wave[1] / n1 * n2)],
                            depth - 1)
-            # wave_propagate(position + 1, [wave[0] * 2 * n1 / (n1 + n2),
+            # wave_propagate(position + 1, [wave[0],
             #                               wave[1] / n1 * n2,
-            #                               w * t + points[position + 1] * (wave[1] - wave[1] / n1 * n2)],
+            #                               wave[2] + points[position + 1] * (wave[1] - wave[1] / n1 * n2)],
             #                depth - 1)
 
             # reflecting
-            wave_propagate(position, [wave[0] / 1.05,
+            wave_propagate(position, [wave[0] * (n1 - n2) / (n1 + n2),
                                       -wave[1],
                                       wave[2] + 2 * wave[1] * points[position + 1]],
                            depth - 1)
-            # waves[position].append([wave[0] / 1.2, -wave[1], wave[2] + 2 * wave[1] * points[position + 1]])
+            # wave_propagate(position, [wave[0] / 1.05,
+            #                           -wave[1],
+            #                           wave[2] + 2 * wave[1] * points[position + 1]],
+            #                depth - 1)
 
         else:
             # propagation <-
@@ -39,28 +42,34 @@ def wave_propagate(position, wave, depth):
             n1 = n[position][0]
             n2 = n[position - 1][0]
             # propagate <-
-            wave_propagate(position - 1, [wave[0]/1.0,
+            wave_propagate(position - 1, [wave[0] * 2 * n1 / (n1 + n2),
                                           wave[1] / n1 * n2,
                                           wave[2] + points[position] * (wave[1] - wave[1] / n1 * n2)],
                            depth - 1)
+            # wave_propagate(position - 1, [wave[0]/1.0,
+            #                               wave[1] / n1 * n2,
+            #                               wave[2] + points[position] * (wave[1] - wave[1] / n1 * n2)],
+            #                depth - 1)
 
             # reflecting
-            # waves[position].append([wave[0] * (n1 - n2) / (n1 + n2), wave[1], wave[2]])
-            wave_propagate(position, [wave[0] / 1.05,
+            wave_propagate(position, [wave[0] * (n1 - n2) / (n1 + n2),
                                       -wave[1],
                                       wave[2] + 2 * wave[1] * points[position]],
                            depth - 1)
-            # waves[position].append([wave[0] / 1.05, -wave[1], wave[2] - 2 * wave[1] * points[position + 1]])
+            # wave_propagate(position, [wave[0] / 1.05,
+            #                           -wave[1],
+            #                           wave[2] + 2 * wave[1] * points[position]],
+            #                depth - 1)
 
 
 c = 1
-w = 2
+w = 1.2
 a = 2
-t = 0
-n1 = 2
-n2 = 6
+t = 13
+n1 = 1
+n2 = 3
 Em = 1
-qual = 100
+qual = 200
 x = linspace(-2, 2, qual)
 #  ->
 E1 = Em * cos(w * t - w * x)
@@ -69,7 +78,7 @@ x3 = linspace(2, 4, qual)
 E3 = 2 * n1 / (n1 + n2) * Em * cos(w * t - w * x3)
 
 # [[eps1, l1], [eps2, l2], [eps3, l3]]
-eps_x = [[n1 ** 2, 2], [n2 ** 2, 2], [n1 ** 2, 2], [n2 ** 2, 2]]
+eps_x = [[n1 ** 2, 2], [n2 ** 2, 2], [n1 ** 2, 2], [n2 ** 2, 2], [n1 ** 2, 2], [n2 ** 2, 2], [n1 ** 2, 2], [n2 ** 2, 2]]
 # [[n1, l1], [n2, l2], [n3, l3]]
 n = deepcopy(eps_x)
 # eps(x) -> n(x)
@@ -87,27 +96,31 @@ points = [0]
 for i in range(len(eps_x)):
     points.append(points[-1] + eps_x[i][1])
 
-wave_propagate(2, [1, -w / c * n[0][0], w * t], 2)
+wave_propagate(0, [Em, -w / c * n[0][0], w * t], 19)
 
 print('points', points)
-print('wawes', waves)
+# print('wawes', waves)
 plt.figure()
 plt.grid(True)
-# plt.plot(x, E1)
-# plt.plot(x, E2, '.-')
-# plt.plot(x3, E3, '.-')
 counter = 0
 for area in waves:
-    y = zeros(100)
-    x = linspace(points[counter], points[counter + 1], 100)
+    qual_in_area = int(qual * eps_x[counter][1] / depth_of_crystal)
+    y = zeros(qual_in_area)
+    x = linspace(points[counter], points[counter + 1], qual_in_area)
     for wave in area:
-        Em = wave[0]
+        Emax = wave[0]
         k = wave[1]
         phase = wave[2]
-        # y = y + Em * cos(k * x + phase)
-        y = Em * cos(k * x + phase)
-        plt.plot(x, y)
-    # plt.plot(x, y)
+        y = y + Emax * cos(k * x + phase)
+        # y = Em * cos(k * x + phase)
+        # plt.plot(x, y)
+    plt.plot(x, y, color='black', linewidth=2)
     counter += 1
 
+# drawing vertical lines for each eps(x)
+for i in points:
+    plt.plot([i, i], [-Em*1.5, Em*1.5], color='black', linewidth=1)
+
+plt.xlabel('x, mkm')
+plt.ylabel('E, V/m')
 plt.show()
